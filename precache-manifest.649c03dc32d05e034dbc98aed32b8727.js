be2211b991d22b9a212fa2ca1c374fd615508657
Saud <saudalardi@outlook.sa>
2020-05-27 18:42:19 +0300

    Updates

---
+++ precache-manifest.649c03dc32d05e034dbc98aed32b8727.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04c5d91f7ddefc60ddca5a81b68c21c2",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "95ea5c2ae10eb35bb877",
+    "url": "/home/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "8a24a212d141a2c2dcc4",
+    "url": "/home/static/css/main.307a871c.chunk.css"
+  },
+  {
+    "revision": "95ea5c2ae10eb35bb877",
+    "url": "/home/static/js/2.448dd0be.chunk.js"
+  },
+  {
+    "revision": "b71cb3cccc73e099d412c24a04bf9105",
+    "url": "/home/static/js/2.448dd0be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a24a212d141a2c2dcc4",
+    "url": "/home/static/js/main.bf827d07.chunk.js"
+  },
+  {
+    "revision": "5c07ecca0456e28e04f0",
+    "url": "/home/static/js/runtime-main.e084aacb.js"
+  },
+  {
+    "revision": "c20218bf99ed7d98023172af19ea79f4",
+    "url": "/home/static/media/resume.c20218bf.pdf"
+  }
+]);
