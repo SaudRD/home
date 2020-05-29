5b8cf877588d64d445c18641a2d0049584ee198e
Saud <saudalardi@outlook.sa>
2020-05-29 15:27:27 +0300

    Updates

---
+++ precache-manifest.5925bc364243091542819b8e503f6db0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02e9af533e6e32e71407d55feac3eba6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95ea5c2ae10eb35bb877",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "ae60ab4f3faac1d81555",
+    "url": "/static/css/main.307a871c.chunk.css"
+  },
+  {
+    "revision": "95ea5c2ae10eb35bb877",
+    "url": "/static/js/2.448dd0be.chunk.js"
+  },
+  {
+    "revision": "b71cb3cccc73e099d412c24a04bf9105",
+    "url": "/static/js/2.448dd0be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae60ab4f3faac1d81555",
+    "url": "/static/js/main.71a50b08.chunk.js"
+  },
+  {
+    "revision": "c88671eb88bf6c28dee9",
+    "url": "/static/js/runtime-main.6a060e8a.js"
+  },
+  {
+    "revision": "c20218bf99ed7d98023172af19ea79f4",
+    "url": "/static/media/resume.c20218bf.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.3f7da0cbe5002e3504717fcee5d9e4cb.js"
+  "/precache-manifest.5925bc364243091542819b8e503f6db0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
